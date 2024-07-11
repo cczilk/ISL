6d75bd06dac9e86b62feb32c05b1973b58d1ba16
cc <cczilk@gmail.com>
2024-07-10 23:34:06 -0700

    sdf

--- send_email.php
+++ send_email.php
@@ -7,12 +7,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
     
-    // Set up email parameters
-    $to = "martinmargiela001@gmail.com"; 
+    // Email recipient
+    $to = "martinmargiela001@gmail.com";
+    
+    // Email subject
     $email_subject = "New Inquiry: " . $subject;
+    
+    // Email body
     $email_body = "Name: $name\n";
     $email_body .= "Email: $email\n";
     $email_body .= "Phone: $phone\n";
+    $email_body .= "Subject: $subject\n";
     $email_body .= "Message:\n$message";
     
     // Headers
@@ -21,8 +26,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // Send email
     if (mail($to, $email_subject, $email_body, $headers)) {
+        // Redirect to a thank you page or display a success message
         echo "Message sent successfully.";
     } else {
+        // Redirect to an error page or display an error message
         echo "Message could not be sent.";
     }
 } else {
